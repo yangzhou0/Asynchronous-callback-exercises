051438c9f28f79ad764d8253f0ae809794562adf
Yang Zhou <yzhou3991@gmail.com>
2018-04-18 20:41:49 -0400

    implement clock app which prints out time elapse

--- w5d5_project_IntroTOCallbacks.js
+++ w5d5_project_IntroTOCallbacks.js
@@ -1,18 +1,44 @@
 class Clock {
   constructor() {
     const now = new Date();
-    this.format = [now.getHours(), now.getMinutes(),now.getSeconds()];
-    this.printTime()
+    this.hr = now.getHours();
+    this.min = now.getMinutes();
+    this.sec = now.getSeconds();
 
+    this.printTime()
+    setInterval(this._tick.bind(this),1000)
 
   }
 
   printTime() {
-    console.log(`${this.format[0]}:${this.format[1]}:${this.format[2]}`);
+    console.log(`${this.hr}:${this.min}:${this.sec}`);
+
   }
 
   _tick() {
-    setInterval(printTime,1000);
+    this.increaseSec();
+    this.printTime();
+  }
+
+  increaseSec(){
+    this.sec += 1;
+    if (this.sec === 60){
+      this.sec = 0;
+      this.increaseMin();
+    }
+  }
+
+  increaseMin(){
+    this.min += 1;
+    if (this.min === 60){
+      this.min =0;
+      this.increaseHr();
+    }
+  }
+
+  increaseHr(){
+    this.hr+=1;
+    this.hr = this.hr%24;
   }
 }
 
