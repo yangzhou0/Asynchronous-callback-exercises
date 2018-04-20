c14cb91f16496fabedfa067a72e1f75b667e4fad
Yang Zhou <yzhou3991@gmail.com>
2018-04-20 01:11:16 -0400

    add in second version of myBind

---
+++ myBind.js
@@ -0,0 +1,35 @@
+Function.prototype.myBind = function (context){return ()=>this.apply(context);} // version1
+Function.prototype.myBind2 = function (context){return this.bind(context);} // version2
+
+class Lamp {
+  constructor() {
+    this.name = "a lamp";
+  }
+}
+
+const turnOn = function() {
+   console.log("Turning on " + this.name);
+}
+
+const lamp = new Lamp();
+
+turnOn(); // should not work the way we want it to
+
+const boundTurnOn = turnOn.bind(lamp);
+const myBoundTurnOn = turnOn.myBind(lamp);
+
+boundTurnOn(); // should say "Turning on a lamp"
+myBoundTurnOn(); // should say "Turning on a lamp"
+
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+
+  meow() {
+    console.log(`${this.name} says meow!`);
+  }
+}
+
+const curie = new Cat("Curie");
+setTimeout(curie.meow.myBind(curie), 1000);
